bc08f10754d3b9b7b7ef8deff3e8e5602ebf048b
Ideasog01 <92227089+Ideasog01@users.noreply.github.com>
2024-02-28 13:08:44 +0000

    Inventory Done!

--- Animal Sanctuary/Assets/Scripts/Manager_Scripts/InventoryManager.cs
+++ Animal Sanctuary/Assets/Scripts/Manager_Scripts/InventoryManager.cs
@@ -43,24 +43,12 @@ public class InventoryManager : MonoBehaviour
                 }
                 else
                 {
-                    Debug.Log("An item exists, but there is no space!");
+                    Debug.Log("An item exists, but there is not enough space!");
                     int room = item.ItemMaxStack - inventoryItem.StackSize;
-                    int leftOver = stackSize - room;
+                    stackSize -= room;
 
-                    if(leftOver > 0)
-                    {
-                        inventoryItem.StackSize = item.ItemMaxStack;
-                        AddItem(item, leftOver);
-                        Debug.Log("There was some items left over, so we are going to try and add the item again");
-                    }
-                    else
-                    {
-                        inventoryItem.StackSize += stackSize;
-                        Debug.Log("There was room for an item in this stack, so we just add the item count to this inventory slot");
-                    }
-
-                    GameManager.InterfaceManager.UpdateInventory(_inventory);
-                    return;
+                    inventoryItem.StackSize = item.ItemMaxStack;
+                    Debug.Log("There was some items left over, so we are going to try and add the item again");
                 }
             }
         }
@@ -84,9 +72,40 @@ public class InventoryManager : MonoBehaviour
 
     public void RemoveItem(Item item, int stackSize)
     {
+        foreach(InventoryItem inventoryItem in _inventory)
+        {
+            if(inventoryItem.Item == item)
+            {
+                if(inventoryItem.StackSize >= stackSize)
+                {
+                    inventoryItem.StackSize -= stackSize;
+                    Debug.Log("There was enough items in this stack to remove the indicated items!");
 
+                    if(inventoryItem.StackSize == 0)
+                    {
+                        Debug.Log("Stack was emptied!");
+                        inventoryItem.Item = null;
+                    }
 
-        //GameManager.InterfaceManager.UpdateInventory(_inventory);
+                    break;
+                }
+                else
+                {
+                    stackSize -= inventoryItem.StackSize;
+                    inventoryItem.StackSize = 0;
+                    inventoryItem.Item = null;
+                    Debug.Log("This stack is now empty, but we want to continue to remove this item.");
+                    continue;
+                }
+            }
+        }
+
+        if(stackSize > 0)
+        {
+            Debug.Log("The items/number of items did not exist in the inventory");
+        }
+
+        GameManager.InterfaceManager.UpdateInventory(_inventory);
     }
 }
 
