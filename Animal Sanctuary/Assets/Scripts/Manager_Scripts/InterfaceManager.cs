82b03e346cec7262b00a6b79ed0a393f8caf20f5
Ideasog01 <92227089+Ideasog01@users.noreply.github.com>
2024-05-29 01:46:53 +0100

    Final Updates

--- Animal Sanctuary/Assets/Scripts/Manager_Scripts/InterfaceManager.cs
+++ Animal Sanctuary/Assets/Scripts/Manager_Scripts/InterfaceManager.cs
@@ -1,9 +1,7 @@
 using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEditor.Progress;
 
 public class InterfaceManager : MonoBehaviour
 {
